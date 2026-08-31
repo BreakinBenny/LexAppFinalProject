4a7a52267ae8f693a688b078fd3c7ca79e3322e0
BreakinBenny <fox@blaj.net>
2026-08-31 16:26:06 +0200

    API should not be included, mistake on my part

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -6,7 +6,6 @@ namespace SistaProjektSeptember2026.Data
 {
 	public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
     {
-        public const string ApiKey = "238306e2";
         public DbSet<TVShow> TVShow { get; set; } = default!;
 		public DbSet<Movie> Movie { get; set; } = default!;
 	}
